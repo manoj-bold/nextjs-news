b6d65fa6f7dba2cc4332c9cb7e18ac091105a78f
Manoj Mishra <manoj.mishra@bold.com>
2024-07-09 04:04:16 +0530

    Add filtering for month

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,23 +1,52 @@
 import NewsList from "@/components/news-list";
 import Link from "next/link";
-import { getAvailableNewsYears } from "@/lib/news";
-import { getNewsForYear } from "@/lib/news";
+import {
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+  getAvailableNewsMonths,
+} from "@/lib/news";
 
 export default function FilteredNewsPage({ params: { filter } }) {
+  const year = filter?.[0];
+  const month = filter?.[1];
+
+  let links, news;
+
+  if (!year) {
+    const years = getAvailableNewsYears();
+    links = years.map((year) => (
+      <li key={year}>
+        <Link href={`/archive/${year}`}>{year}</Link>
+      </li>
+    ));
+  } else {
+    const months = getAvailableNewsMonths(year);
+    links = months.map((month) => (
+      <li key={month}>
+        <Link href={`/archive/${year}/${month}`}>{month}</Link>
+      </li>
+    ));
+
+    if (month) {
+      news = getNewsForYearAndMonth(year, month);
+    } else {
+      news = getNewsForYear(year);
+    }
+  }
+
   return (
     <>
       <header id="archive-header">
         <nav>
-          <ul>
-            {getAvailableNewsYears().map((year) => (
-              <li key={year}>
-                <Link href={`/archive/${year}`}>{year}</Link>
-              </li>
-            ))}
-          </ul>
+          <ul>{links}</ul>
         </nav>
       </header>
-      {filter?.[0] && <NewsList news={getNewsForYear(filter[0])} />}
+      {news && news.length > 0 ? (
+        <NewsList news={news} />
+      ) : (
+        <>No news found for the selected year.</>
+      )}
     </>
   );
 }
