29b09d29c7239b68457f688230d2952bed85e4d3
Manoj Mishra <manoj.mishra@bold.com>
2024-07-09 11:01:06 +0530

    Add client side data fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,22 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
+
+import { useEffect, useState } from "react";
 import NewsList from "@/components/news-list";
 
 export default function AllNewsPage() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/news")
+      .then((response) => response.json())
+      .then((data) => setNews(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div id="news">
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </div>
   );
 }
